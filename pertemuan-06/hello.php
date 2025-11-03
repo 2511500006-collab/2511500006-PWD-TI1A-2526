59ecba112440c53ea93338a9b1cdd4b12a15ec5a
2511500006-Grezelco Govin <2511500006>
2025-11-03 19:58:47 +0700

    Latihan array dan penggunaan print_r() dan var_dump()

--- pertemuan-06/hello.php
+++ pertemuan-06/hello.php
@@ -89,10 +89,20 @@ default: echo "Hari biasa.";
 }
 ?>
 <?php
-$hobi = ["Coding", "Memasak", "Musik"];
-foreach ($hobi as $item) {
-echo "Hobi: $item <br>";
+$hobi = ["Coding", "Memasak", "Musik", "Membaca", "Traveling"];
+echo "<h3>Daftar Hobi Saya:</h3>";
+for ($i = 0; $i < count($hobi); $i++) {
+echo ($i + 1) . ". " . $hobi[$i] . "<br>";
 }
+echo "<hr>";
+echo "<h4>Hasil print_r():</h4>";
+echo "<pre>";
+print_r($hobi);
+echo "</pre>";
+echo "<h4>Hasil var_dump():</h4>";
+echo "<pre>";
+var_dump($hobi);
+echo "</pre>";
 ?>
 </body>
 </html>
